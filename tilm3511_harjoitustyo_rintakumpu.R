849acccaa1c9be86719eff07c7f9605bdff3d248
Lasse Rintakumpu <rintakumpu@gmail.com>
2014-03-13 21:06:49 +0200

    *Almost finished R*

--- tilm3511_harjoitustyo_rintakumpu.R
+++ tilm3511_harjoitustyo_rintakumpu.R
@@ -18,7 +18,8 @@ library(kirjasto, character.only = TRUE)
 
 # Ladataan/asennetaan käytetyt kirjastot
 
-lapply(c("moments","car", "alr3"), lataa_kirjasto)
+#lapply(c("moments","car", "alr3"), lataa_kirjasto)
+lapply(c("lawstat", "car"), lataa_kirjasto)
 
 # Ladataan havaintoaineisto
 
@@ -32,167 +33,112 @@ talous <- read.csv("https://raw.github.com/rintakumpu/tilm3511/master/talous.csv
 
 talous_koulutustaso <- subset(talous, koulutus != 1)
 
-# Kuvaillaan dataa laatikko-janakuviolla
+# Kuvaillaan dataa sironta- ja laatikko-janakuviolla
 
-boxplot(talous_koulutustaso$saasto94~talous_koulutustaso$koulutus)
+plot(talous_koulutustaso$koulutus, talous_koulutustaso$saasto94)
+plot(talous_koulutustaso$saasto94~as.factor(talous_koulutustaso$koulutus))
 
-# Jakaumat näyttävät kohtuullisen samoilta, testataan varianssien homogeenisuutta
+# Oletetaan havainnot toisistaan riippumattomiksi. Testataan varianssien homogeenisuutta.
+# Jakaumat eivät näytä normaaleilta. Käytetään testaukseen Brown-Forsythea.
+# Tehdään testi tasolla 0.05.
+# H0: Luokkien varianssi sama.
+# Hv: Luokkien varianssi eri.
+
+levene.test(talous_koulutustaso$saasto94, as.factor(talous_koulutustaso$koulutus))
+
+# Test Statistic = 1.1104, p-value = 0.3464 
+# => Hylätään Hv tasolla 0.05. Varianssit ovat samat.
+
+# Jakaumat eivät näytä normaaleilta, tarkastellaan normaalisuutta.
+qqnorm(talous_koulutustaso$saasto94)
+shapiro.test(talous_koulutustaso$saasto94)
+
+# W = 0.8943, p-value = 7.199e-10
+# => Normaalisuusoletus ei päde. Haetaan dataan optimaalinen potenssimuunnos.
+
+pt_saasto94 <- powerTransform(talous_koulutustaso$saasto94)
+# -0.001575101
+shapiro.test(talous_koulutustaso$saasto94^-0.001575101)
+# W = 0.995, p-value = 0.8181
+# => Näyttää huomattavasti paremmalta. Jatketaan muunnetuilla arvolla.
+
+saasto94_muunnettu <- (talous_koulutustaso$saasto94^-0.001575101)
+
+# Jatketaan säästöerojen tarkastelua. Testataan erojen tilastollista merkitsevyyttä tasolla 0.05.
+
+# H0: Säästöissä ei eroa.
+# Hv: Säästöissä on eroa.
+# Tehdään ainoistolle yksisuuntainen varianssianalyysi.
+
+reg <- aov(saasto94_muunnettu~as.factor(talous_koulutustaso$koulutus))
+summary(reg)
+
+#                                          Df    Sum Sq   Mean Sq F value Pr(>F)
+# as.factor(talous_koulutustaso$koulutus)   3 3.680e-06 1.227e-06   2.011  0.114
+# Residuals                               172 1.049e-04 6.101e-07   
+
+# => F-testisuure antaa p-arvon 0.114. Hylätään vastahypoteesi tasolla 0.05.
+# Koulutustasolla ei ole tilastollisesti merkitsevää vaikutusta säästöihin,
+# kun vain perus/kansakoulun suorittaneet rajataan tarkastelun ulkopuolelle.
 
 
-
-
-
-
-# Käytetään normaalikvantiilikuvaajaa sekä laatikko-janakuviota
-
+# TODO: Tulosta kaaviot.
 boxplot(nettotulot_miehet, nettotulot_naiset, col = c("skyblue", "pink"), xaxl="n", ylab = "€/kk")
 axis(1, at = c(1,2), labels = c("Miehet", "Naiset"))
-
-# Maksimoidaan kuvaajien luotettavuus tallentamalla pdf-muodossa
-# http://xkcd.com/1301/
-
 pdf('boxplot_tulot.pdf')
 dev.off()
 
-skewness(nettotulot_miehet)
-kurtosis(nettotulot_miehet)
-# Miehet: g1, 0.3483119, g2: 2.343662
-skewness(nettotulot_naiset)
-kurtosis(nettotulot_naiset)
-# Naiset: g2, 0.4554618, g2: 2.343662
-
-# Haetaan potenssimuunnosta paremman normaalijakautuneisuuden löytämiseksi
-
-ptm <- powerTransform(nettotulot_miehet)
-# Estimated transformation parameters 
-# nettotulot_miehet 
-# 0.4683207 
-
-ptn <- powerTransform(nettotulot_naiset)
-# Estimated transformation parameters 
-# nettotulot_naiset 
-# 0.4279722 
-
-# Sovelletaan molempiin aineistoihin neliöjuurimuunnosta
-
-nettotulot_m_muunnettu <- nettotulot_miehet^0.5
-nettotulot_n_muunnettu <- nettotulot_naiset^0.5
-
-skewness(nettotulot_m_muunnettu)
-kurtosis(nettotulot_m_muunnettu)
-# Miehet: g1: -0.01, g2: 2.35
-skewness(nettotulot_n_muunnettu)
-kurtosis(nettotulot_n_muunnettu)
-# Naiset: g1: 0.005, g2: 2.58
-
-qqnorm(nettotulot_m_muunnettu, main = "Miehet, muunnettu ^0.5")
-pdf('qqnorm_m_muunnettu.pdf')
-dev.off()
-qqnorm(nettotulot_n_muunnettu, main = "Naiset, muunnettu ^0.5")
-pdf('qqnorm_n_muunnettu.pdf')
-dev.off()
-
-# Otoskeskiarvot viittaavat tuloeroihin sukupuolten välillä.
-
-nettotulot_m_viiva <- mean(nettotulot_miehet) # 5118.921
-nettotulot_n_viiva <- mean(nettotulot_naiset) # 4391.956
-
-# Testataan onko miesten ja naisten nettotuloissa tilastollista eroa.
-
-# Valitaan nollahypoteesiksi H0: mu_m - mu_n == 0 (populaatioiden odotusarvot samat)
-# ja vastahypoteesiksi       Hv: mu_m - mu_n != 0
-
-t.test(nettotulot_m_muunnettu, nettotulot_n_muunnettu, alternative = "two.sided", conf.level = 0.95)
-
-# data:  nettotulot_m_muunnettu and nettotulot_n_muunnettu
-# t = 2.7782, df = 174.866, p-value = 0.006064
-# alternative hypothesis: true difference in means is not equal to 0
-# 95 percent confidence interval:
-#  1.480589 8.744629
-# sample estimates:
-#   mean of x mean of y 
-# 70.36786  65.25525 
-
-# => Hylätään nollahypoteesi riskitasolla 0.05. Testin p-arvosta näemme,
-# että pienen riskitaso, jolla testi voitaisiin hylätä on 0.006064.
 
 ##############################
 # 2. Iän vaikutus säästöihin #
 ##############################
 
 # Kuvaillaan iän vaikutusta säästöihin sirontakuviolla
-
-plot(talous$saasto94~talous$ika)
+plot(talous$ika, talous$saasto94)
 
 # Sirontakuvion perusteella iällä ja säästöillä näyttäisi olevan korrelaatio
-# Varmennetaan tämä:
+# Varmennetaan tämä: Saadaan merkitsevä positiivinen korrelaatio.
+cor(talous$ika, talous$saasto94) # 0.5726042
+cor.test(talous$ika, talous$saasto94) # p-value < 2.2e-16
 
 # Sovitetaan aineistoon lineaarinen regressiomalli
-
 reg <- lm(talous$saasto94~talous$ika)
-plot(reg)
-summary(reg)
 
-# Lineaarinen regressio on herkkä poikkeaville havainnoille
+# (Intercept)   talous$ika  
+# -5286         1057 
+abline(-5286, 1057, col="red")
+
+summary(reg)
+#             Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)  -5285.6     4363.6  -1.211    0.227    
+# talous$ika    1057.1      113.4   9.318   <2e-16 ***
+
+# Adjusted R-squared:  0.3241.
+# F-statistic: 86.83 on 1 and 178 DF,  p-value: < 2.2e-16
+# => Malli on tilastollisesti merkitsevä. Kulmakerroin ei kuitenkaan ole.
+# => F-testisuure kertoo iän vaikuttavan merkitsevästi säästöihin. 
+
+# Lineaarinen regressio on herkkä poikkeaville havainnoille.
+plot(reg)
 # Sirontakuviosta huomataan, että havainnot 11, 116, 178 ovat selkeästi poikkeavia
 
-# Poistetaan havainnot ja sovitetaan suora uudelleen:
+# Käytetään jälleen dataan haettavaa optimia potenssimuunnosta normaalisuuden parantimseksi
+pt_saasto94 <- powerTransform(talous$saasto94) # 0.1838458
+plot(talous$saasto94^0.1838458~talous$ika)
+reg_muunnettu <- lm(talous$saasto94^0.1838458~talous$ika) # 5.20707 / 0.03949 
+abline(5.20707,0.03949, col="blue")
+summary(reg_muunnettu)
 
-# Testataan mallin sopivuutta
+#             Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) 5.207066   0.150147   34.68   <2e-16 ***
+# talous$ika  0.039491   0.003903   10.12   <2e-16 ***
+# Adjusted R-squared:  0.3615 
+# F-statistic: 102.4 on 1 and 178 DF,  p-value: < 2.2e-16
 
+# Nyt mallin vakiotermi ja kulmakerroin ovat molemmat merkitseviä,
+# lisäksi jälkimmäisen mallin korjattu selitysaste (ja F-testisuure) on hieman korkeampi.
 
-
-# Käytetään aineiston kuvailuun histogrammia sekä ryhmäpylväskuviota.
-
-tyytyvaisyys_m <- hist(miehet$taltyyt, 0:4, breaks = c(0.5,1.5,2.5,3.5,4.5))
-plot(tyytyvaisyys_m,
-     xlab="Tyytyväisyys\n(1: Erittäin tyytyväinen - 4: Erittäin tyytymätön)",
-     ylab="f",
-     main="Miesten tyytyväisyys taloudelliseen tilanteeseen",
-     col="skyblue")
-
-pdf('tyytyvaisyys_mies.pdf')
-dev.off()
-
-tyytyvaisyys_n <- hist(naiset$taltyyt, 0:4, breaks = c(0.5,1.5,2.5,3.5,4.5))
-plot(tyytyvaisyys_n,
-     xlab="Tyytyväisyys\n(1: Erittäin tyytyväinen - 4: Erittäin tyytymätön)",
-     ylab="f",
-     main="Naisten tyytyväisyys taloudelliseen tilanteeseen",
-     col="pink", ylim = c(0,40))
-
-pdf('tyytyvaisyys_nainen.pdf')
-dev.off()
-
-tyytyvaisyys_bp <- barplot(tyytyvaisyys, beside = TRUE, 
-        main=c("Tyytyväisyys taloudelliseen tilanteeseen"),
-        col=c("skyblue","pink"), 
-        ylab="f", legend.text=c("Miehet","Naiset"), xlim = c(0,10))
-text(tyytyvaisyys_bp, 0, round(tyytyvaisyys, 1),cex=1,pos=3) 
-
-pdf('tyytyvaisyys_yhteinen.pdf')
-dev.off()
-
-# Lasketaan estimaatit
-
-pm <- tyytyvaisyys[1,1] / sum(tyytyvaisyys[1,]) # 0.5842697
-pn <- tyytyvaisyys[2,1] / sum(tyytyvaisyys[2,]) # 0.4395604
-
-# Testataan suhteellisten osuuksien erotusta tasolla 0.05
-# Valitaan vastahypoteesiksi tehtävänannon mukaan
-# oletus, että miehet ovat naisia tyytyväisempiä.
-# H0: pm-pn = 0 / Hv: pm-pn > 0 
-
-prop.test(tyytyvaisyys, alternative = "greater", conf.level=0.95)
-
-# data:  tyytyvaisyys
-# X-squared = 3.2138, df = 1, p-value = 0.03651
-# alternative hypothesis: greater
-# 95 percent confidence interval:
-#  0.0123195 1.0000000
-# sample estimates:
-#  prop 1    prop 2 
-# 0.5842697 0.4395604 
-
-# Pienin arvo, jolla nollahypoteesi voitaisiin
-# hylätä on 0.03651, hylätään nollahypoteesi tasolla 0.05.
+# Vaihtoehtoisesti voidaan myös yrittää poistaa 
+# poikkeavia havaintoja ja sovittaa suoraa uudelleen.
+# Kumpiakin malleja voidaan kuitenkin jo pitää riittävän hyvinä,
+# joten jätetään alkuperäinen data rauhaan.
