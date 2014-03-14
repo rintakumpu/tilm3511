0150e150748e5c2621fc4bec81d10607e5750161
Lasse Rintakumpu <rintakumpu@gmail.com>
2014-03-14 10:52:55 +0200

    Tweaked power transformations
    Exports for plots (+ clean plots) still needed.

--- tilm3511_harjoitustyo_rintakumpu.R
+++ tilm3511_harjoitustyo_rintakumpu.R
@@ -1,11 +1,13 @@
-# TILM3511 -harjoitustyö, R-koodi
-# Lasse Rintakumpu, 63555
-# 11.3.2013
+###################################
+# TILM3511 -harjoitustyö, R-koodi #
+# Lasse Rintakumpu, 63555         #
+# 14.3.2013                       #
+###################################
 
 # Asetetaan työhakemisto
 
 wd <- "C:/Users/Lasse Rintakumpu/Documents/GitHub/tilm3511"
-wd <- "D:/Dropbox/Edu/Statistics/Peruskurssi C/Harjoitustyö/"
+# wd <- "D:/Dropbox/Edu/Statistics/Peruskurssi C/Harjoitustyö/"
 setwd(wd)
 
 # Funktio kirjastojen asentamiselle / lataamiselle
@@ -18,8 +20,7 @@ library(kirjasto, character.only = TRUE)
 
 # Ladataan/asennetaan käytetyt kirjastot
 
-#lapply(c("moments","car", "alr3"), lataa_kirjasto)
-lapply(c("lawstat", "car"), lataa_kirjasto)
+lapply(c("lawstat", "car", "moments"), lataa_kirjasto)
 
 # Ladataan havaintoaineisto
 
@@ -35,7 +36,9 @@ talous_koulutustaso <- subset(talous, koulutus != 1)
 
 # Kuvaillaan dataa sironta- ja laatikko-janakuviolla
 
-plot(talous_koulutustaso$koulutus, talous_koulutustaso$saasto94)
+plot(talous_koulutustaso$koulutus, talous_koulutustaso$saasto94,
+     xlab="Koulutustaso", ylab="Säästöt vuonna 1994, euroa",
+     col=c("blue","red","magenta","black"), xlim=)
 plot(talous_koulutustaso$saasto94~as.factor(talous_koulutustaso$koulutus))
 
 # Oletetaan havainnot toisistaan riippumattomiksi. Testataan varianssien homogeenisuutta.
@@ -70,8 +73,8 @@ saasto94_muunnettu <- (talous_koulutustaso$saasto94^-0.001575101)
 # Hv: Säästöissä on eroa.
 # Tehdään ainoistolle yksisuuntainen varianssianalyysi.
 
-reg <- aov(saasto94_muunnettu~as.factor(talous_koulutustaso$koulutus))
-summary(reg)
+malli1 <- aov(saasto94_muunnettu~as.factor(talous_koulutustaso$koulutus))
+summary(malli1)
 
 #                                          Df    Sum Sq   Mean Sq F value Pr(>F)
 # as.factor(talous_koulutustaso$koulutus)   3 3.680e-06 1.227e-06   2.011  0.114
@@ -80,7 +83,6 @@ summary(reg)
 # => F-testisuure antaa p-arvon 0.114. Hylätään vastahypoteesi tasolla 0.05.
 # Koulutustasolla ei ole tilastollisesti merkitsevää vaikutusta säästöihin,
 # kun vain perus/kansakoulun suorittaneet rajataan tarkastelun ulkopuolelle.
-
 
 # TODO: Tulosta kaaviot.
 boxplot(nettotulot_miehet, nettotulot_naiset, col = c("skyblue", "pink"), xaxl="n", ylab = "€/kk")
@@ -102,13 +104,13 @@ cor(talous$ika, talous$saasto94) # 0.5726042
 cor.test(talous$ika, talous$saasto94) # p-value < 2.2e-16
 
 # Sovitetaan aineistoon lineaarinen regressiomalli
-reg <- lm(talous$saasto94~talous$ika)
+malli2 <- lm(talous$saasto94~talous$ika)
 
 # (Intercept)   talous$ika  
 # -5286         1057 
 abline(-5286, 1057, col="red")
 
-summary(reg)
+summary(malli2)
 #             Estimate Std. Error t value Pr(>|t|)    
 # (Intercept)  -5285.6     4363.6  -1.211    0.227    
 # talous$ika    1057.1      113.4   9.318   <2e-16 ***
@@ -119,26 +121,36 @@ summary(reg)
 # => F-testisuure kertoo iän vaikuttavan merkitsevästi säästöihin. 
 
 # Lineaarinen regressio on herkkä poikkeaville havainnoille.
-plot(reg)
 # Sirontakuviosta huomataan, että havainnot 11, 116, 178 ovat selkeästi poikkeavia
+# Ja jäännösten tarkastelu kertoo, etteivät ne ole normaalijakautuneet:
+plot(malli2)
+qqnorm(resid(malli2))
+skewness(resid(malli2)) # 1.729508
 
 # Käytetään jälleen dataan haettavaa optimia potenssimuunnosta normaalisuuden parantimseksi
-pt_saasto94 <- powerTransform(talous$saasto94) # 0.1838458
-plot(talous$saasto94^0.1838458~talous$ika)
-reg_muunnettu <- lm(talous$saasto94^0.1838458~talous$ika) # 5.20707 / 0.03949 
-abline(5.20707,0.03949, col="blue")
-summary(reg_muunnettu)
+# Potenssimuunnos voidaan tehdä, koska kaikki havainnot ovat positiivisia kokonaislukuja.
+pt_saasto94 <- powerTransform(malli2) 
+#Y1 
+#0.2057932 
+malli2b <- lm(bcPower(talous$saasto94, pt_saasto94$roundlam)~talous$ika) # Y1 | 0.33
+# => Saadaan vakiotermi ja kulmakerroin: 52.8542  0.9741  
+
+plot(talous$ika, bcPower(talous$saasto94, pt_saasto94$roundlam))
+abline(52.8542, 0.9742, col="blue")
+
+summary(malli2b)
 
 #             Estimate Std. Error t value Pr(>|t|)    
 # (Intercept) 5.207066   0.150147   34.68   <2e-16 ***
 # talous$ika  0.039491   0.003903   10.12   <2e-16 ***
-# Adjusted R-squared:  0.3615 
-# F-statistic: 102.4 on 1 and 178 DF,  p-value: < 2.2e-16
+# Adjusted R-squared:  0.3622 
+# F-statistic: 102.7 on 1 and 178 DF,  p-value: < 2.2e-16
 
 # Nyt mallin vakiotermi ja kulmakerroin ovat molemmat merkitseviä,
-# lisäksi jälkimmäisen mallin korjattu selitysaste (ja F-testisuure) on hieman korkeampi.
+# lisäksi jälkimmäisen mallin korjattu selitysaste ja F-testisuure ovat hieman korkeampia.
 
 # Vaihtoehtoisesti voidaan myös yrittää poistaa 
 # poikkeavia havaintoja ja sovittaa suoraa uudelleen.
-# Kumpiakin malleja voidaan kuitenkin jo pitää riittävän hyvinä,
+
+# Kumpiakin malleja voidaan kuitenkin pitää riittävän hyvinä,
 # joten jätetään alkuperäinen data rauhaan.
